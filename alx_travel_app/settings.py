6aaff1fbbbfe8a92b6d6efc7609f7132a4f71ac2
Mohameddek1 <mohadek2004@gmail.com>
2025-07-20 19:28:22 +0300

    first commit

--- alx_travel_app/settings.py
+++ alx_travel_app/settings.py
@@ -67,15 +67,10 @@ WSGI_APPLICATION = 'alx_travel_app.wsgi.application'
 # MySQL Database configuration
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': env('DB_NAME'),
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASSWORD'),
-        'HOST': env('DB_HOST'),
-        'PORT': env('DB_PORT'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
-
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
